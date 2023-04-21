53a3250def6f0baabf6dbd574a61af5e8f585ac2
Arash <88967049+ara6i@users.noreply.github.com>
2023-04-21 12:15:22 +0330

    typography

---
+++ src/components/TypoGraphy.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+function TypoGraphy() {
+  return (
+    <div>
+      <Typography variant="h1" component="h1" gutterBottom>
+        Hello
+      </Typography>
+      <Typography variant="h2">Hello</Typography>
+      <Typography variant="h3">Hello</Typography>
+      <Typography variant="subtitle2">Hello</Typography>
+    </div>
+  );
+}
+
+export default TypoGraphy;
